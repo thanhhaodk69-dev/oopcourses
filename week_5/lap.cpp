22ea8d9c7dfb043f5bd18ef27e28c7e8fb7697d7
thanhhaodk69-dev <thanhhaodk69@gmail.com>
2026-09-09 10:36:38 +0700

    exerise

--- week_5/lap.cpp
+++ week_5/lap.cpp
@@ -187,5 +187,22 @@ int main() {
         }
     }
     cout << "Longest pass streak: " << maxStreak << " students\n";
+    // Phân bố điểm dạng biểu đồ sao
+    int c1 = 0, c2 = 0, c3 = 0, c4 = 0, c5 = 0;
+
+    for (int i = 0; i < n; i++) {
+        if (scores[i] < 5.0) c1++;
+        else if (scores[i] < 6.5) c2++;
+        else if (scores[i] < 8.0) c3++;
+        else if (scores[i] < 9.0) c4++;
+        else c5++;
+    }
+
+    cout << "\n===== SCORE DISTRIBUTION =====\n";
+    cout << "0 - <5   : "; for (int i = 0; i < c1; i++) cout << "*"; cout << " " << c1 << "\n";
+    cout << "5 - <6.5 : "; for (int i = 0; i < c2; i++) cout << "*"; cout << " " << c2 << "\n";
+    cout << "6.5 - <8 : "; for (int i = 0; i < c3; i++) cout << "*"; cout << " " << c3 << "\n";
+    cout << "8 - <9   : "; for (int i = 0; i < c4; i++) cout << "*"; cout << " " << c4 << "\n";
+    cout << "9 - 10   : "; for (int i = 0; i < c5; i++) cout << "*"; cout << " " << c5 << "\n";
     return 0;
 }
